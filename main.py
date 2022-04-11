9d0ffc7bef369b8b71f8f238a2038c6b7e51a70a
Jiin Kim <jik0808@gmail.com>
2022-04-11 10:20:53 -0400

    add DMV consumption

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ def main():
                        page_icon='💡',
                        initial_sidebar_state='expanded')
 
+    # Add sidebar title.
+    st.sidebar.title(body='Options')
+
+    # Sidebar select box to choose sector.
+    sector = st.sidebar.selectbox(label='Select a sector:',
+                                  options=('Total Consumption', 'Residential Sector', 'Commercial Sector',
+                                           'Industrial Sector', 'Transportation Sector'))
+
+    # Sidebar year slider.
+    year = st.sidebar.slider(label='Select a year:',
+                             min_value=1960,
+                             max_value=2019)
+
     # Print title.
     st.title(body='💡 INST 490 Capstone Project')
 
@@ -21,10 +34,6 @@ def main():
     # Write description.
     st.write('Comprehensive state-level estimates of energy production, consumption, prices, and expenditures by '
              'source and sector.')
-
-    # Select box to choose sector.
-    sector = st.selectbox(label='Select a sector:',
-                          options=('Total Consumption', 'Residential Sector', 'Commercial Sector', 'Industrial Sector', 'Transportation Sector'))
 
     # Read total energy consumption data.
     df = pd.read_excel(io=r'use_tot_sector.xlsx',
@@ -41,11 +50,6 @@ def main():
             axis=0,
             inplace=True)
 
-    # Year slider.
-    year = st.slider(label='Select a year:',
-                     min_value=1960,
-                     max_value=2019)
-
     # Create figure.
     fig = px.choropleth(data_frame=df,
                         locations='State',
@@ -58,17 +62,34 @@ def main():
     # Print choropleth map figure to page.
     st.write(fig)
 
+    # Drops states except DMV.
+    dmv_df = df[df['State'].isin(['DC', 'MD', 'VA'])]
+
+    # Create figure.
+    fig = px.choropleth(data_frame=dmv_df,
+                        locations='State',
+                        locationmode='USA-states',
+                        color=year,
+                        scope='usa',
+                        title=f'Total Energy Consumption Estimates DMV {sector} {year}',
+                        labels={f'{year}': 'Billion Btu'})
+
+    fig.update_geos(fitbounds='locations')
+
+    # Print choropleth map figure to page.
+    st.write(fig)
+
     # Set header for raw data.
     st.header(body='Raw Data')
 
     # Print DataFrame.
-    file_container = st.expander('Display Raw Data .xlsx')
+    file_container = st.expander('Display Total Energy Consumption Data .xlsx')
     file_container.write(df)
 
     # Save raw data button to save DataFrame as CSV file.
     st.download_button(label='Press to Download Raw Data',
                        data=df.to_csv(),
-                       file_name='file.csv',
+                       file_name=f'{sector}.csv',
                        mime='text/csv',
                        key='download-csv')
 
