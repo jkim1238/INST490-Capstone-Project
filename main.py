ab9872aea9da193c2eb8d96acee376a680fa191c
Jiin Kim <jik0808@gmail.com>
2022-04-16 23:27:05 -0400

    add more UX

--- main.py
+++ main.py
@@ -8,19 +8,6 @@ def main():
     st.set_page_config(page_title='INST 490 Capstone Project',
                        page_icon='💡')
 
-    # Add sidebar title.
-    st.sidebar.title(body='Options')
-
-    # Sidebar select box to choose sector.
-    sector = st.sidebar.selectbox(label='Select a sector:',
-                                  options=('Total Consumption', 'Residential Sector', 'Commercial Sector',
-                                           'Industrial Sector', 'Transportation Sector'))
-
-    # Sidebar year slider.
-    year = st.sidebar.slider(label='Select a year:',
-                             min_value=1960,
-                             max_value=2019)
-
     # Print title.
     st.title(body='💡 INST 490 Capstone Project')
 
@@ -46,17 +33,16 @@ def main():
     st.header(body='Table of Contents')
 
     # The contents.
-    contents = '- [Summary](#summary)\n'\
+    contents = '- [Summary](#summary)\n' \
                '- [Choose Dataset](#choose-dataset)\n'
 
     # If dataset is energy consumption.
     if visualization == 'Energy Consumption':
-        contents += '- [Data Visualization](#data-visualization)\n'\
-                    '  - [Raw Data](#raw-data)\n'\
-                    '  - [Choropleth Map](#choropleth-map)\n'\
-                    '  - [Line Plot](#line-plot)\n'\
-                    '- [Machine Learning](#machine-learning)\n'\
-                    '  - [Equation](#equation)\n'\
+        contents += '- [Data Visualization](#data-visualization)\n' \
+                    '  - [Choropleth Map](#choropleth-map)\n' \
+                    '  - [Line Plot](#line-plot)\n' \
+                    '- [Machine Learning](#machine-learning)\n' \
+                    '  - [Equation](#equation)\n' \
                     '  - [Prediction](#prediction)'
 
     st.markdown(body=contents, unsafe_allow_html=True)
@@ -72,32 +58,34 @@ def main():
         st.write('Comprehensive state-level estimates of energy production, consumption, prices, and expenditures by '
                  'source and sector.')
 
+        # Subheader for choropleth map.
+        st.subheader('Choropleth Map')
+
+        # Set 2 columns for the options.
+        col1, col2 = st.columns(2)
+
+        with col1:
+            # Select box to choose sector.
+            sector = st.selectbox(label='Select a sector:',
+                                  options=('Total Consumption', 'Residential Sector', 'Commercial Sector',
+                                           'Industrial Sector', 'Transportation Sector'),
+                                  key='sector1')
+        with col2:
+            # Year slider.
+            year = st.slider(label='Select a year:',
+                             min_value=1960,
+                             max_value=2019,
+                             key='slider1')
+
         # Read total energy consumption data.
         df = pd.read_excel(io=r'use_tot_sector.xlsx',
                            sheet_name=sector,
                            header=2)
 
-        # Set header for raw data.
-        st.subheader(body='Raw Data')
-
-        # Print DataFrame.
-        file_container = st.expander(label='Display Total Energy Consumption Data .xlsx')
-        file_container.write(df)
-
-        # Save raw data button to save DataFrame as CSV file.
-        st.download_button(label='Press to Download Raw Data',
-                           data=df.to_csv(),
-                           file_name=f'{sector}.csv',
-                           mime='text/csv',
-                           key='download-csv')
-
         # Drop first row.
         df.drop(index=df.index[0],
                 axis=0,
                 inplace=True)
-
-        # Copy for scatter plot later.
-        scatter_df = df.copy()
 
         # Drop last row.
         df.drop(index=df.index[-1],
@@ -113,15 +101,53 @@ def main():
                             title=f'Total Energy Consumption Estimates U.S. {sector} {year}',
                             labels={f'{year}': 'Billion Btu'})
 
-        # Subheader for choropleth map.
-        st.subheader('Choropleth Map')
-
         # Print choropleth map figure to page.
         st.write(fig)
 
+        # Print DataFrame.
+        file_container = st.expander(label=f'Click to display Total Energy Consumption U.S. {sector} {year} Data')
+        file_container.write(df)
+
+        # Save raw data button to save DataFrame as CSV file.
+        st.download_button(label='Press to Download Raw Data',
+                           data=df.to_csv(),
+                           file_name=f'{sector}.csv',
+                           mime='text/csv',
+                           key='download-csv1')
+
+        # Set 2 columns for the options.
+        col1, col2 = st.columns(2)
+
+        with col1:
+            # Select box to choose sector.
+            sector = st.selectbox(label='Select a sector:',
+                                  options=('Total Consumption', 'Residential Sector', 'Commercial Sector',
+                                           'Industrial Sector', 'Transportation Sector'),
+                                  key='sector2')
+        with col2:
+            # Year slider.
+            year = st.slider(label='Select a year:',
+                             min_value=1960,
+                             max_value=2019,
+                             key='slider2')
+
+        # Read total energy consumption data.
+        df = pd.read_excel(io=r'use_tot_sector.xlsx',
+                           sheet_name=sector,
+                           header=2)
+
+        # Drop first row.
+        df.drop(index=df.index[0],
+                axis=0,
+                inplace=True)
+
+        # Drop last row.
+        df.drop(index=df.index[-1],
+                axis=0,
+                inplace=True)
+
         # Drops states except DMV.
         dmv_df = df[df['State'].isin(['DC', 'MD', 'VA'])]
-        scatter_df = scatter_df[scatter_df['State'].isin(['DC', 'MD', 'VA', 'US'])]
 
         # Create choropleth map figure.
         fig = px.choropleth(data_frame=dmv_df,
@@ -137,22 +163,84 @@ def main():
         # Print choropleth map figure to page.
         st.write(fig)
 
+        # Print DataFrame.
+        file_container = st.expander(label=f'Click to display Total Energy Consumption DMV {sector} {year} Data')
+        file_container.write(df)
+
+        # Save raw data button to save DataFrame as CSV file.
+        st.download_button(label='Press to Download Raw Data',
+                           data=df.to_csv(),
+                           file_name=f'{sector}.csv',
+                           mime='text/csv',
+                           key='download-csv2')
+
+        # Subheader for line plot.
+        st.subheader('Line Plot', anchor='line-plot')
+
+        # Set 2 columns for the options.
+        col1, col2 = st.columns(2)
+
+        with col1:
+            # Select box to choose sector.
+            sector = st.selectbox(label='Select a sector:',
+                                  options=('Total Consumption', 'Residential Sector', 'Commercial Sector',
+                                           'Industrial Sector', 'Transportation Sector'),
+                                  key='sector3')
+        with col2:
+            # Slider for year range.
+            years = st.slider(label='Select a year range:',
+                              min_value=1960,
+                              max_value=2019,
+                              value=(1960, 2019),
+                              key='range1')
+
+        # Read total energy consumption data.
+        df = pd.read_excel(io=r'use_tot_sector.xlsx',
+                           sheet_name=sector,
+                           header=2)
+
+        # Drop first row.
+        df.drop(index=df.index[0],
+                axis=0,
+                inplace=True)
+
+        # Drop last row.
+        df.drop(index=df.index[-1],
+                axis=0,
+                inplace=True)
+
+        # Drops states except DMV.
+        dmv_df = df[df['State'].isin(['DC', 'MD', 'VA'])]
+
         # Make years a column for plotting.
         dmv_df = pd.melt(dmv_df, id_vars=['State'], var_name='Year')
+
+        # Filter between year range.
+        dmv_df = dmv_df[dmv_df['Year'] >= years[0]]
+        dmv_df = dmv_df[dmv_df['Year'] <= years[1]]
 
         # Create line plot figure.
         fig = px.line(data_frame=dmv_df,
                       x='Year',
                       y='value',
                       color='State',
-                      title=f'Total Energy Consumption Estimates DMV {sector} 1960-2019',
+                      title=f'Total Energy Consumption Estimates DMV {sector} {years[0]}-{years[1]}',
                       labels={'value': 'Energy Consumption (Billion Btu)'})
-
-        # Subheader for line plot.
-        st.subheader('Line Plot', anchor='line-plot')
 
         # Print line plot figure to page.
         st.write(fig)
+
+        # Print DataFrame.
+        file_container = st.expander(label=f'Click to display Total Energy Consumption DMV {sector} '
+                                           f'{years[0]}-{years[1]} Data')
+        file_container.write(dmv_df)
+
+        # Save raw data button to save DataFrame as CSV file.
+        st.download_button(label='Press to Download Raw Data',
+                           data=dmv_df.to_csv(),
+                           file_name=f'{sector}.csv',
+                           mime='text/csv',
+                           key='download-csv3')
 
         # Header for machine learning.
         st.header(body='Machine Learning', anchor='machine-learning')
@@ -160,22 +248,56 @@ def main():
         # Subheader for linear regression model.
         st.subheader(body='Scatter Plot with Linear Regression Model', anchor='linear-regression-model')
 
-        # Select box to choose state.
-        state = st.selectbox(label='Select a state:',
-                             options=('US', 'DC', 'MD', 'VA'))
+        # Set 2 columns for the options.
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            # Select box to choose state.
+            state = st.selectbox(label='Select a state:',
+                                 options=('US', 'DC', 'MD', 'VA'))
+
+        with col2:
+            # Select box to choose sector.
+            sector = st.selectbox(label='Select a sector:',
+                                  options=('Total Consumption', 'Residential Sector', 'Commercial Sector',
+                                           'Industrial Sector', 'Transportation Sector'),
+                                  key='sector4')
+
+        with col3:
+            # Slider for year range.
+            years = st.slider(label='Select a year range:',
+                              min_value=1960,
+                              max_value=2019,
+                              value=(1960, 2019),
+                              key='range2')
+
+        # Read total energy consumption data.
+        scatter_df = pd.read_excel(io=r'use_tot_sector.xlsx',
+                                   sheet_name=sector,
+                                   header=2)
+
+        # Drop first row.
+        scatter_df.drop(index=df.index[0],
+                        axis=0,
+                        inplace=True)
 
         # Drop other states.
+        scatter_df = scatter_df[scatter_df['State'].isin(['DC', 'MD', 'VA', 'US'])]
         scatter_df = scatter_df[scatter_df['State'] == state]
 
         # Make years a column for plotting.
         scatter_df = pd.melt(scatter_df, id_vars=['State'], var_name='Year')
+
+        # Filter between year range.
+        scatter_df = scatter_df[scatter_df['Year'] >= years[0]]
+        scatter_df = scatter_df[scatter_df['Year'] <= years[1]]
 
         # Create scatter plot figure.
         fig = px.scatter(data_frame=scatter_df,
                          x='Year',
                          y='value',
                          color='State',
-                         title=f'Total Energy Consumption Estimates {state} {sector} 1960-2019',
+                         title=f'Total Energy Consumption Estimates {state} {sector} {years[0]}-{years[1]}',
                          labels={'value': 'Energy Consumption (Billion Btu)'},
                          trendline='ols',
                          trendline_color_override='red')
@@ -186,6 +308,22 @@ def main():
         # Get OLS results.
         results = px.get_trendline_results(fig)
 
+        # Print summary.
+        file_container = st.expander(label='Click to display Model Parameters')
+        file_container.write(results.px_fit_results.iloc[0].summary())
+
+        # Print DataFrame.
+        file_container = st.expander(label=f'Click to display Total Energy Consumption {state} {sector} '
+                                           f'{years[0]}-{years[1]} Data')
+        file_container.write(scatter_df)
+
+        # Save raw data button to save DataFrame as CSV file.
+        st.download_button(label='Press to Download Raw Data',
+                           data=dmv_df.to_csv(),
+                           file_name=f'{sector}.csv',
+                           mime='text/csv',
+                           key='download-csv4')
+
         # Get coefficients.
         b = results.iloc[0]['px_fit_results'].params[0]
         m = results.iloc[0]['px_fit_results'].params[1]
@@ -194,27 +332,31 @@ def main():
         st.subheader(body='Equation')
 
         # Print equation.
-        st.write(state, ' ', sector)
+        st.write(state, ' ', sector, ' ', years[0], '-', years[1])
         st.write('y = ', m, 'x + ', b)
 
         # Print subheader for prediction.
         st.subheader(body='Prediction', anchor='prediction')
 
+        year = st.number_input(label='Enter a year:',
+                               value=2022,
+                               format='%d')
+
         # Print explanation.
         st.write('Using Ordinary Least Squares (OLS) linear regression model, we predict that ', state, ' ', sector,
-                 ' will consume ', (m * 2022 + b), ' billion Btu of energy in the ', sector, ' for 2022.')
+                 ' will consume ', (m * year + b), ' billion Btu of energy in the ', sector, ' for ', year, '.')
 
-        # Print header for data analysis.
-        st.header(body='Data Analysis')
+    # Print header for data analysis.
+    st.header(body='Data Analysis')
 
-        # Print analysis.
-        st.write('TODO')
+    # Print analysis.
+    st.write('TODO')
 
-        # Print header for Insights and Recommendations.
-        st.header(body='Insights and Recommendations')
+    # Print header for Insights and Recommendations.
+    st.header(body='Insights and Recommendations')
 
-        # Print analysis.
-        st.write('TODO')
+    # Print analysis.
+    st.write('TODO')
 
 
 if __name__ == '__main__':
