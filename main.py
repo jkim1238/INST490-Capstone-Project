3fecde310430057e72e50aaaccbb85ccec3bf36b
Jiin Kim <jik0808@gmail.com>
2022-05-02 20:07:00 -0400

    remove Tableau

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-from io import StringIO
-
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-import tableauserverclient as TSC
-
-# Set up Tableau connection.
-tableau_auth = TSC.PersonalAccessTokenAuth(
-    st.secrets['tableau']['token_name'],
-    st.secrets['tableau']['token_secret'],
-    st.secrets['tableau']['site_id']
-)
-server = TSC.Server(st.secrets["tableau"]["server_url"], use_server_version=True)
 
 
 def main():
@@ -43,7 +32,7 @@ def main():
 
     # Select box to choose dataset.
     visualization = st.selectbox(label='Select a dataset:',
-                                 options=('Energy Consumption', 'Energy Usage Price', 'Tableau Online'))
+                                 options=('Energy Consumption', 'Energy Usage Price'))
 
     # Table of contents.
     st.sidebar.title(body='Table of Contents')
@@ -581,32 +570,6 @@ def main():
         st.write('Using Ordinary Least Squares (OLS) linear regression model, we predict that the Average Price ',
                  state, ' ', sector, ' Sector ', provider, ' will cost ', calculate_prediction(m, year, b),
                  ' Cents/kWh in ', year, '.')
-    elif visualization == 'Tableau Online':
-        # Subheader for Tableau Online.
-        st.subheader(body='Tableau Online')
-
-        # Tableau description.
-        st.write('We can connect to Tableau Online and use their')
-
-        workbooks_names, views_names, view_name, view_image, view_csv = run_query()
-
-        # Print results.
-        st.subheader("📓 Workbooks")
-        st.write("Found the following workbooks:", ", ".join(workbooks_names))
-
-        st.subheader("👁️ Views")
-        st.write(
-            f"Workbook *{workbooks_names[0]}* has the following views:",
-            ", ".join(views_names),
-        )
-
-        st.subheader("🖼️ Image")
-        st.write(f"Here's what view *{view_name}* looks like:")
-        st.image(view_image, width=300)
-
-        st.subheader("📊 Data")
-        st.write(f"And here's the data for view *{view_name}*:")
-        st.write(pd.read_csv(StringIO(view_csv)))
 
     # Print header for data analysis.
     st.header(body='Data Analysis',
@@ -632,7 +595,11 @@ def main():
     - The minimum amount of energy that Virginia consumed was 683,534 billion BTU.
     - The average price of energy use in Virginia cost $7,341,56
     """)
-    st.markdown('###### Insights & Findings ')
+
+    # Print header.
+    st.markdown(body='###### Insights & Findings ')
+
+    # Print insights.
     st.write(""" 
     Based on the data analysis, we can make the inference that the DMV region has experienced 
     an increase in energy consumption and energy usage price. This means that, with the current 
@@ -643,8 +610,11 @@ def main():
     # Print header/subheader for Recommendations.
     st.header(body='Recommendations',
               anchor='recommendations')
+
     # Print markdown
-    st.markdown('###### State-Level Energy Efficiency Programs')
+    st.markdown(body='###### State-Level Energy Efficiency Programs')
+
+    # Print DMV.
     st.write("""
     -	MD
     	- PEPCO - Commercial and Industrial Energy Efficiency Rebate Program
@@ -658,8 +628,9 @@ def main():
     """)
 
     # Print markdown
-    st.markdown('###### State-Level Energy Efficiency Policies')
+    st.markdown(body='###### State-Level Energy Efficiency Policies')
 
+    # Print policies.
     st.write("""
     -	Encourage retro-commissioning when it comes to developing and building public and private buildings.
     -	Implement state-wide energy performance building codes for the numerous commercial and residential buildings constructed or in the process of construction.
@@ -857,32 +828,6 @@ def get_scatter_price_df(sector, provider, state):
 @st.cache
 def calculate_prediction(m, year, b):
     return m * year + b
-
-
-# Get various data.
-# Explore the tableauserverclient library for more options.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def run_query():
-    with server.auth.sign_in(tableau_auth):
-        # Get all workbooks.
-        workbooks, pagination_item = server.workbooks.get()
-        workbooks_names = [w.name for w in workbooks]
-
-        # Get views for first workbook.
-        server.workbooks.populate_views(workbooks[0])
-        views_names = [v.name for v in workbooks[0].views]
-
-        # Get image & CSV for first view of first workbook.
-        view_item = workbooks[0].views[0]
-        server.views.populate_image(view_item)
-        server.views.populate_csv(view_item)
-        view_name = view_item.name
-        view_image = view_item.image
-        # `view_item.csv` is a list of binary objects, convert to str.
-        view_csv = b"".join(view_item.csv).decode("utf-8")
-
-        return workbooks_names, views_names, view_name, view_image, view_csv
 
 
 if __name__ == '__main__':
